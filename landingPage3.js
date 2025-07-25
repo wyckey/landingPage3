6cff433af283ded626c37e188fd6158cc7173f31
wyckey <hungarian365@gmail.com>
2025-07-25 17:16:38 +0200

    Add files via upload

---
+++ landingPage3.js
@@ -0,0 +1,22 @@
+function toggleIngredients() {
+  const list = document.getElementById("ingredientList");
+  list.classList.toggle("d-none");
+}
+
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();

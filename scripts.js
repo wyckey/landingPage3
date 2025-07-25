8423a85e629ab6de86dc475ac407d873bf2325e9
vluemoon <hungarian365@gmail.com>
2025-07-25 21:25:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,20 +3,3 @@ function toggleIngredients() {
   list.classList.toggle("d-none");
 }
 
-(function () {
-  "use strict";
-  var forms = document.querySelectorAll(".needs-validation");
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
